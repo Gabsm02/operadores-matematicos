0f5028abdfd5e2b0fd79625ed7f0e5960b6d5549
Gabriel Mendes <gabrielomendes02@gmail.com>
2024-04-30 13:44:15 -0300

    Aprendendo sobre operadores matemáticos

---
+++ operadores.py
@@ -0,0 +1,24 @@
+# Soma
+soma = 1 + 3 + 4
+print(soma)
+
+# Subtração
+subtracao = 10 - 5
+print(subtracao)
+
+# Divisão
+divisao = 10 / 2
+print(divisao)
+
+# Exponenciação
+exponenciacao = 2 ** 3
+print(exponenciacao)
+
+# Resto da divisão
+resto = 10 % 3
+
+# Divisão de chão
+divisao_chao = 10 // 3
+
+# Priorizaão
+prioridade = (1 + 3) * 4
